212f5f3c0489918a06b0fb04f9ec985bd6becbe1
saroj poudel <saroj.poudel@fmr.com>
2019-07-09 11:45:58 -0600

    update form components and add slider-floating-filter

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormioForm, FormioOptions} from 'angular-formio';
 
 @Component({
@@ -7,12 +7,116 @@ import {FormioForm, FormioOptions} from 'angular-formio';
   styleUrls: ['./forms.component.scss']
 })
 export class FormsComponent implements OnInit {
-  public form: FormioForm = { components: [] };
+  // public form: FormioForm = { components: [] };
   public formOptions: FormioOptions = {};
-  constructor() { }
+
+  form = {
+    components: [{
+      input: true,
+      tableView: true,
+      inputType: 'text',
+      inputMask: '',
+      label: 'First Name',
+      key: 'firstName',
+      placeholder: 'Enter your first name',
+      prefix: '',
+      suffix: '',
+      multiple: false,
+      defaultValue: '',
+      protected: false,
+      unique: false,
+      persistent: true,
+      validate: {
+        required: false,
+        minLength: '',
+        maxLength: '',
+        pattern: '',
+        custom: '',
+        customPrivate: false
+      },
+      conditional: {
+        show: false,
+        when: null,
+        eq: ''
+      },
+      type: 'textfield'
+    }, {
+      input: true,
+      tableView: true,
+      inputType: 'text',
+      inputMask: '',
+      label: 'Last Name',
+      key: 'lastName',
+      placeholder: 'Enter your last name',
+      prefix: '',
+      suffix: '',
+      multiple: false,
+      defaultValue: '',
+      protected: false,
+      unique: false,
+      persistent: true,
+      validate: {
+        required: false,
+        minLength: '',
+        maxLength: '',
+        pattern: '',
+        custom: '',
+        customPrivate: false
+      },
+      conditional: {
+        show: false,
+        when: null,
+        eq: ''
+      },
+      type: 'textfield'
+    }, {
+      input: true,
+      tableView: true,
+      label: 'Message',
+      key: 'message',
+      placeholder: 'What do you think?',
+      prefix: '',
+      suffix: '',
+      rows: 3,
+      multiple: false,
+      defaultValue: '',
+      protected: false,
+      persistent: true,
+      validate: {
+        required: false,
+        minLength: '',
+        maxLength: '',
+        pattern: '',
+        custom: ''
+      },
+      type: 'textarea',
+      conditional: {
+        show: false,
+        when: null,
+        eq: ''
+      }
+    }, {
+      type: 'button',
+      theme: 'primary',
+      disableOnInvalid: true,
+      action: 'submit',
+      block: false,
+      rightIcon: '',
+      leftIcon: '',
+      size: 'md',
+      key: 'submit',
+      tableView: false,
+      label: 'Submit',
+      input: true
+    }]
+  };
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
+
   formChanged(event: any) {
     console.log('form changed', event);
   }
--- src/app/grid/slider-floating-filter/slider-floating-filter.component.ts
+++ src/app/grid/slider-floating-filter/slider-floating-filter.component.ts
@@ -11,7 +11,7 @@ export interface SliderFloatingFilterParams extends IFloatingFilterParams {
 @Component({
   template: `
         <input type="range"
-               min="30000" [max]="maxValue"
+               min="25000" [max]="maxValue"
                data-show-value="true" data-popup-enabled="true"
                [(ngModel)]="currentValue"
                (ngModelChange)="valueChanged()"/>`
@@ -26,11 +26,12 @@ export class SliderFloatingFilterComponent implements IFloatingFilter, AgFramewo
   agInit(params: SliderFloatingFilterParams): void {
     this.params = params;
     this.maxValue = this.params.maxValue;
-    this.currentValue = 20000;
+    console.log(this.maxValue);
+    this.currentValue = 25000;
   }
 
   valueChanged() {
-    const valueToUse = (this.currentValue === 20000) ? null : this.currentValue;
+    const valueToUse = (this.currentValue === 25000) ? null : this.currentValue;
     this.params.parentFilterInstance( instance => {
       (instance as NumberFilter).onFloatingFilterChanged('greaterThan', valueToUse);
     });
